0accde3f177e3325f921be2eb17d9a1df932d938
Jermyiah <escobarjeremy@csu.fullerton.edu>
2023-08-30 15:38:58 +0000

    updated

--- src/utils/routes.js
+++ src/utils/routes.js
@@ -3,10 +3,6 @@ import Home from "../components/Home";
 import Projects from "../components/Projects";
 import PageNotFound from "./../components/404";
 import Accessibiity from "./../components/Accessibiity";
-import Bloging from "../components/Bloging";
-import Jobing from "../components/Jobing";
-import Ordering from "../components/Ordering";
-import Howing from "../components/Howtoing";
 
 export const routes = [
   {
@@ -28,18 +24,6 @@ export const routes = [
   {
     path: "/bloging",
     element: Bloging,
-  },
-  {
-    path: "/jobing",
-    element: Jobing,
-  },
-  {
-    path: "/ordering",
-    element: Ordering,
-  },
-  {
-    path: "/howtoing",
-    element: Howing,
   },
   {
     path: "accessibiity",
